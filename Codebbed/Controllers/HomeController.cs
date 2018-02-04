961ae9e1f6242c7778da7e40a289e75b0ffec9e5
akm151 <akm151@gmail.com>
2018-02-05 00:38:43 +0530

    Mail Functionality

--- Codebbed/Controllers/HomeController.cs
+++ Codebbed/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using Newtonsoft.Json;
 
 namespace Codebbed.Controllers
 {
@@ -15,6 +17,37 @@ namespace Codebbed.Controllers
         {
             return View();
         }
+        public ActionResult SendMail()
+        {
 
+            MailMessage mailMessage = new MailMessage("codebbed@gmail.com", "akm151@gmail.com");
+            MailAddress cc1 = new MailAddress("saumya093@gmail.com");
+            MailAddress cc2 = new MailAddress("pallavibhandari1993@gmail.com");
+            mailMessage.CC.Add(cc1);
+            mailMessage.CC.Add(cc2);
+            mailMessage.Subject = "test email";
+            mailMessage.Body = "this is the body of test mail";
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "codebbed@gmail.com",
+                Password = "test"
+            };
+            smtpClient.EnableSsl = true;
+            try
+            { 
+            smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error", new { message = ex.Message });
+            }
+            return Json(new {isSuccess=true, message = "Thanks for Contacting Us.We will get back to you shortly." },JsonRequestBehavior.AllowGet);
+
+        }
     }
+
+
 }
+
